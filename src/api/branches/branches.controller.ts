5cc97187de2692cfa6fd689ca6f7667d254c2703
jamshidbekml <jamshidbekml@gmail.com>
2024-08-10 20:13:22 +0500

    initial commit

--- src/api/branches/branches.controller.ts
+++ src/api/branches/branches.controller.ts
@@ -11,7 +11,13 @@ import {
 import { BranchesService } from './branches.service';
 import { CreateBranchDto } from './dto/create-branch.dto';
 import { UpdateBranchDto } from './dto/update-branch.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiBearerAuth()
 @ApiTags('branches')
@@ -19,28 +25,33 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 export class BranchesController {
   constructor(private readonly branchesService: BranchesService) {}
 
-  @Post()
-  create(@Body() createBranchDto: CreateBranchDto) {
-    return this.branchesService.create(createBranchDto);
-  }
+  // @Post()
+  // @ApiOperation({ summary: 'Create branch' })
+  // @ApiBody({ type: CreateBranchDto })
+  // create(@Body() createBranchDto: CreateBranchDto) {
+  //   return this.branchesService.create(createBranchDto);
+  // }
 
   @Get()
+  @ApiOperation({ summary: 'Get all branches' })
+  @ApiQuery({ name: 'page', type: Number, required: false, example: 1 })
+  @ApiQuery({ name: 'limit', type: Number, required: false, example: 10 })
   findAll(@Query('page') page = 1, @Query('limit') limit = 10) {
     return this.branchesService.findAll(page, limit);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.branchesService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.branchesService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBranchDto: UpdateBranchDto) {
-    return this.branchesService.update(+id, updateBranchDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateBranchDto: UpdateBranchDto) {
+  //   return this.branchesService.update(+id, updateBranchDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.branchesService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.branchesService.remove(+id);
+  // }
 }
