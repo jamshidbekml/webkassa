fb4ff1af7c1a44ae95481c464b07ffe6498c10ad
jamshidbekml <jamshidbekml@gmail.com>
2024-08-15 14:08:43 +0500

    initial commit

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Body, Req } from '@nestjs/common';
+import { Controller, Body, Req, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { GetMe, SignIn } from './decorators/auth.decorator';
 import { ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
+import { Public } from './decorators/public.decorator';
+import { RefreshTokenGuard } from './guards/refreshToken.guard';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -19,5 +21,14 @@ export class AuthController {
   getMe(@Req() req: Request) {
     const obj = req['user'] as { username: string };
     return this.authService.getMe(obj.username);
+  }
+
+  @Public()
+  @UseGuards(RefreshTokenGuard)
+  @Get('refresh')
+  refreshTokens(@Req() req: Request) {
+    const userId = req['user']['sub'];
+    const refreshToken = req['user']['refreshToken'];
+    return this.authService.refreshTokens(userId, refreshToken);
   }
 }
