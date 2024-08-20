e2ed18d8972bdd1f99f8dd5eacd09dfb18550687
jamshidbekml <jamshidbekml@gmail.com>
2024-08-20 18:17:34 +0500

    initial commit

--- src/api/contracts/dto/contract.dto.ts
+++ src/api/contracts/dto/contract.dto.ts
@@ -25,35 +25,35 @@ export class ContractResponse {
   @Expose()
   updatedAt: Date;
 
-  //   @Transform(
-  //     ({ value }) => {
-  //       return value
-  //         ? {
-  //             id: value.product.id,
-  //             barcode: value.product.barcode,
-  //             psid: value.product.catalogcode,
-  //             name: value.product.name,
-  //             packageCode: value.product.packagecode,
-  //             vat: Number(value.product.vat) / 100,
-  //             price: value.amount,
-  //             amount: value.count,
-  //             discountAmount: value.discountAmount,
-  //             labels:
-  //               value.labels.length > 0
-  //                 ? value.labels.map((label) => label.label)
-  //                 : [],
-  //             units: 'шт',
-  //             isDecimalUnits: false,
-  //             unitCode: null,
-  //             commissionPINFL: null,
-  //             commissionTIN: null,
-  //           }
-  //         : null;
-  //     },
-  //     {
-  //       toClassOnly: true,
-  //     },
-  //   )
   @Expose()
+  @Transform(
+    ({ value }) => {
+      return value;
+      // ? {
+      //     id: value.product.id,
+      //     barcode: value.product.barcode,
+      //     psid: value.product.catalogcode,
+      //     name: value.product.name,
+      //     packageCode: value.product.packagecode,
+      //     vat: Number(value.product.vat) / 100,
+      //     price: value.amount,
+      //     amount: value.count,
+      //     discountAmount: value.discountAmount,
+      //     labels:
+      //       value.labels.length > 0
+      //         ? value.labels.map((label) => label.label)
+      //         : [],
+      //     units: 'шт',
+      //     isDecimalUnits: false,
+      //     unitCode: null,
+      //     commissionPINFL: null,
+      //     commissionTIN: null,
+      //   }
+      // : null;
+    },
+    {
+      toClassOnly: true,
+    },
+  )
   products: object[];
 }
