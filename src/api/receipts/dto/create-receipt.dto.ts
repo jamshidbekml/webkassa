f71a202bd93303edbdf61a8e5e6dc48460935ac9
jamshidbekml <jamshidbekml@gmail.com>
2024-08-22 17:53:59 +0500

    initial commit

--- src/api/receipts/dto/create-receipt.dto.ts
+++ src/api/receipts/dto/create-receipt.dto.ts
@@ -1,14 +1,56 @@
 import { PAYMENT_TYPE, RECEIPT_TYPE } from '@prisma/client';
+import {
+  IsArray,
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 
 export class CreateReceiptDto {
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   cTin: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   cName: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsNumber()
   tAmount: number;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsNumber()
   tVat: number;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   saleId: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   type: RECEIPT_TYPE;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   contractId: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
   createdAt?: Date;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsArray()
   payments: { amount: number; type: PAYMENT_TYPE }[];
 }
 
